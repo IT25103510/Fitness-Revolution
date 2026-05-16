b821d37f29622afe5737894a110f426f33778df7
It25101887 <It25101887@my.sliit.lk>
2026-05-17 00:22:28 +0530

    Update AttendanceController with new attendance management features

--- fitness-revolution/src/main/java/com/fitnessRevolution/Reporting-System/controller/AttendanceController.java
+++ fitness-revolution/src/main/java/com/fitnessRevolution/Reporting-System/controller/AttendanceController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/attendance")
