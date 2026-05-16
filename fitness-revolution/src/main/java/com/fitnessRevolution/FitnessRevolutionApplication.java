d3f8451bba23938ad512a3a8c63022998f4a8ee2
It25101887 <It25101887@my.sliit.lk>
2026-05-16 23:31:03 +0530

    Project Main files

---
+++ fitness-revolution/src/main/java/com/fitnessRevolution/FitnessRevolutionApplication.java
@@ -0,0 +1,11 @@
+package com.fitnessRevolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessRevolutionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessRevolutionApplication.class, args);
+    }
+}
